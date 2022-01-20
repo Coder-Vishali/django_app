98abe9185b50c11563769745245861b5b7534f6d
Vishali Srinivasan <60731083+Coder-Vishali@users.noreply.github.com>
2022-01-20 11:10:31 +0530

    Update urls.py

--- django_app/urls.py
+++ django_app/urls.py
@@ -16,4 +16,5 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 urlpatterns = [path('test_app/', include('test_app.urls')),
+               path('', views.index, name='index'),
                path('admin/', admin.site.urls), ]
